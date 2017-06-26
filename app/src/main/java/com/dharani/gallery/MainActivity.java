5d21d016b988d0f8bf20667a0667639ba7cb5a3b
Dharani16 <rdharani1606@gmail.com>
2017-06-26 09:38:43 +0530

    Initial commit

---
+++ app/src/main/java/com/dharani/gallery/MainActivity.java
@@ -0,0 +1,99 @@
+package com.dharani.gallery;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Cursor cursor;
+    private int columnIndex;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Set up an array of the Thumbnail Image ID column we want
+        String[] projection = {MediaStore.Images.Thumbnails._ID};
+        // Create the cursor pointing to the SDCard
+        cursor = managedQuery( MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI,
+                projection, // Which columns to return
+                null,       // Return all rows
+                null,
+                MediaStore.Images.Thumbnails.IMAGE_ID);
+        // Get the column index of the Thumbnails Image ID
+        columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Thumbnails._ID);
+        GridView sdcardImages = (GridView) findViewById(R.id.sdcard);
+        sdcardImages.setAdapter(new ImageAdapter(this));
+
+        sdcardImages.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Get the data location of the image
+                String[] projection = {MediaStore.Images.Media.DATA};
+                cursor = managedQuery( MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        projection, // Which columns to return
+                        null,       // Return all rows
+                        null,
+                        null);
+                columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                cursor.moveToPosition(position);
+                // Get image filename
+                String imagePath = cursor.getString(columnIndex);
+                // Use this path to do further processing, i.e. full screen display
+            }
+        });
+    }
+
+    private class ImageAdapter extends BaseAdapter {
+        private Context context;
+        public ImageAdapter(Context localContext) {
+            context = localContext;
+        }
+        public int getCount() {
+            return cursor.getCount();
+        }
+        public Object getItem(int position) {
+            return position;
+        }
+        public long getItemId(int position) {
+            return position;
+        }
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView picturesView;
+
+            if (convertView == null) {
+                picturesView = new ImageView(context);
+                // Move cursor to current position
+                cursor.moveToPosition(position);
+                // Get the current value for the requested column
+                int imageID = cursor.getInt(columnIndex);
+                // Set the content of the image based on the provided URI
+                picturesView.setImageURI(Uri.withAppendedPath(
+                        MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI, "" + imageID));
+                picturesView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                picturesView.setPadding(8, 8, 8, 8);
+                picturesView.setLayoutParams(new GridView.LayoutParams(100, 100));
+            }
+            else {
+                picturesView = (ImageView)convertView;
+            }
+            return picturesView;
+        }
+    }
+
+
+
+
+
+}
